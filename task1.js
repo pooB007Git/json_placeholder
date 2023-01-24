85708769150416b5b905fcb16bc32a05efed8e14
pooB007Git <bhagatpooja100@gmail.com>
2023-01-24 21:24:59 +0530

    Made with class

--- task1.js
+++ task1.js
@@ -1,31 +1,36 @@
 import fetch from 'node-fetch';
 import fs from "fs";
 
-function arrayToCSV (data) {
-    const csv = data.map(row => Object.values(row));
-    console.log("CSV");
-    console.log(csv);
-    csv.unshift(Object.keys(data[0]));
-    return `"${csv.join('"\n"').replace(/,/g, '","')}"`;
-}
+const CreateCSVClass = class {
+    constructor( api_path , file_path ) {
+        this.file_path = file_path;
+        this.api_path = api_path;
+    }
 
-async function writeCSV (fileName, data) {
-    try {
-        fs.appendFileSync(fileName, data); 
-    } catch (err) {
-      console.log(err);
-      process.exit(1);
+    fetchData = async() => {
+        const response = await fetch(this.api_path);
+        const data = await response.json();
+        return data;
+    }
+    arrayToCSV = async(data) => {
+        console.log(data)
+        const csv = data.map(row => Object.values(row));
+        csv.unshift(Object.keys(data[0]));
+        return `"${csv.join('"\n"').replace(/,/g, '","')}"`;
+    }
+
+    writeCSV = async( fileName, data ) => {
+        try {
+            fs.appendFileSync(fileName, data); 
+        } catch (err) {
+        console.log(err);
+        process.exit(1);
+        }
     }
 }
+const class_csv = new CreateCSVClass( "https://jsonplaceholder.typicode.com/posts" , "./CSV_files/postdata.csv" )
+const data = await class_csv.fetchData();
+const CSV = await class_csv.arrayToCSV(data);
+class_csv.writeCSV(class_csv.file_path, CSV);
+console.log(`Successfully converted ${class_csv.file_path}!`);
 
-(async () => {
-    const outputFileName = './CSV_files/postdata.csv';
-
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json();
-
-  	// const data = await parseJSONFile(inputFileName);
-  	const CSV = arrayToCSV(data);
-  	await writeCSV(outputFileName, CSV);
-	console.log(`Successfully converted ${outputFileName}!`);
-})();
